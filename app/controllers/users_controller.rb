84b0d50ca059c514f7635f6c907128ccd8a7974e
Kent <kentkdunn@gmail.com>
2016-09-20 05:34:04 -0700

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,18 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def show
     @user = User.find(params[:id])
   end
 
-   def new
+  def new
     @user = User.new
   end
 
-   def create
+  def create
     @user = User.new(user_params)
     if @user.save
-    	flash[:success] = "Welcome to the Sample App!"
-    	redirect_to @user
-      # Handle a successful save.
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
  
   
-  get 'static_pages/home'
-
-  get  '/help',    to: 'static_pages#help', as: 'help'
+  
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'static_pages#home'
+   
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
